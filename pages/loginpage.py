1e2a50ec4659bf4e35a80784a3bfbf28f0c7b244
Pranjal <pranjalmelkani18@gmail.com>
2025-03-16 13:23:52 +0530

    login page added

---
+++ pages/loginpage.py
@@ -0,0 +1,23 @@
+from base.basedriver import Basedriver
+
+
+class Loginpage(Basedriver):
+    USERNAME_FIELD = "//*[@placeholder='Username']"
+    PASSWORD_FIELD = "//*[@placeholder='Password']"
+    LOGIN_BTN = "//*[contains(@class, 'orangehrm-login-button')]"
+    FORGOT_PASSWORD_LINK = "//*[contains(@class, 'orangehrm-login-forgot-header')]"
+
+    def __init__(self, driver):
+        super().__init__(driver)
+    
+    def enter_username(self, username):
+        self.send_keys(self.USERNAME_FIELD, username)
+
+    def enter_password(self, password):
+        self.send_keys(self.PASSWORD_FIELD, password)
+
+    def click_on_login(self):
+        self.click_on_element(self.LOGIN_BTN)
+    
+    def click_on_forgot_password(self):
+        self.click_on_element(self.FORGOT_PASSWORD_LINK)
