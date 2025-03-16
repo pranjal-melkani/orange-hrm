b31342b52683417c142d0dea45c350ab5fb45770
Pranjal <pranjalmelkani18@gmail.com>
2025-03-16 14:23:25 +0530

    invalid login logic added

--- pages/loginpage.py
+++ pages/loginpage.py
@@ -6,6 +6,9 @@ class Loginpage(Basedriver):
     PASSWORD_FIELD = "//*[@placeholder='Password']"
     LOGIN_BTN = "//*[contains(@class, 'orangehrm-login-button')]"
     FORGOT_PASSWORD_LINK = "//*[contains(@class, 'orangehrm-login-forgot-header')]"
+    INVALID_CRENTIALS_ERROR = "//*[text()='Invalid credentials']"
+    BLANK_USERNAME_ERROR = "//*[@placeholder='Username']//parent::div//parent::div//*[text()='Required']"
+    BLANK_PASSWORD_ERROR = "//*[@placeholder='Password']//parent::div//parent::div//*[text()='Required']"
 
     def __init__(self, driver):
         super().__init__(driver)
@@ -25,6 +28,13 @@ class Loginpage(Basedriver):
     def is_logged_in(self):
         try:
             self.wait_until_element_is_visible("//header//*[text()='Dashboard']")
+            return True
+        except Exception:
+            return False
+    
+    def invalid_credentials_error_visible(self):
+        try:
+            self.wait_until_element_is_visible(self.INVALID_CRENTIALS_ERROR)
             return True
         except Exception:
             return False
