d81400db3a12284cefbe48a468eefc9fcb7c4050
Pranjal <pranjalmelkani18@gmail.com>
2025-03-16 14:36:28 +0530

    blank credentials added

--- testcases/test_login.py
+++ testcases/test_login.py
@@ -4,6 +4,27 @@ from pages.dashboardpage import Dashboardpage
 
 
 class Testlogin:
+    def test_blank_entries(self, driver):
+        lp = Loginpage(driver)
+        lp.click_on_login()
+        username_error = lp.enter_username_error_visible()
+        password_error = lp.enter_password_error_visible()
+        if username_error and password_error:
+            assert True
+        else:
+            assert False
+    
+    def test_invalid_credentials(self, driver):
+        username = "asd"
+        password = "asdsad"
+
+        lp = Loginpage(driver)
+        lp.enter_username(username)
+        lp.enter_password(password)
+        lp.click_on_login()
+        error_visible = lp.invalid_credentials_error_visible()
+        assert error_visible
+
     def test_valid_login(self, driver):
         username = "Admin"
         password = "admin123"
@@ -17,14 +38,3 @@ class Testlogin:
         dp = Dashboardpage(driver)
         dp.logout()
         assert is_logged_in
-        
-    def test_invalid_credentials(self, driver):
-        username = "asd"
-        password = "asdsad"
-
-        lp = Loginpage(driver)
-        lp.enter_username(username)
-        lp.enter_password(password)
-        lp.click_on_login()
-        error_visible = lp.invalid_credentials_error_visible()
-        assert error_visible
